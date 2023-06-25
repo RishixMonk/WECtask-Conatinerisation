30a2e656d8862d9830d97eac80bc8edab3155db8
RishixMonk <mrrishi.201me131@nitk.edu.in>
2023-06-25 15:25:04 +0530

    minor changes

--- .secrets/deploy.sh
+++ .secrets/deploy.sh
@@ -13,14 +13,14 @@ source venv/bin/activate
 echo "Virtual env 'mb' Activated !"
 
 echo "Installing Dependencies..."
-pip install -r requirements.txt --no-input
+pip3 install -r requirements.txt --no-input
 
 echo "Serving Static Files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Running Database migration"
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Deactivate Virtual Env
 deactivate
